9dfe96929394ace59789fc8badc657557417eedc
tomavidhi04 <vidhi.tomar@zucol.in>
2025-11-10 16:26:09 +0530

    late

--- src/components/PagesComp/Blog/BlogMainHeroSection.tsx
+++ src/components/PagesComp/Blog/BlogMainHeroSection.tsx
@@ -6,7 +6,7 @@ export default function BlogMainHeroSection() {
         <section className="relative h-screen bg-gradient-to-b from-[#00191c] via-[#000] to-[#000] lg:h-[90vh] flex items-end lg:items-center overflow-hidden border-b border-gray-200 rounded-t-none md:rounded-t-2xl rounded-b-2xl">
 
             <video
-                className="absolute object-cover rotate-[90deg]  mt-[1%]  right-[20%]  "
+                className="absolute object-cover rotate-[90deg] h-full lg:h-auto   mt-[1%]  right-[-1%]  "
                 src="/backgrounds/Blog.mp4"
                 autoPlay
                 loop
@@ -19,6 +19,8 @@ export default function BlogMainHeroSection() {
                 fill
                 className="object-cover mix-blend-hue pointer-events-none opacity-[94%]"
             />
+                                    <div className="pointer-events-none absolute inset-y-0 left-0 w-full top-0 bg-gradient-to-l  from-transparent to-black/100" />
+
             <div className="relative z-10 w-full px-6 md:px-16 lg:px-24 max-w-[90rem] mx-auto pb-30 md:pb-60 lg:pb-0 lg:pt-32">
                 <HeadingPurple title="[Transforming AI Services]" />
                 <h1 className="  text-[32px] md:text-[32px] lg:text-[40px] font-light font-[300] leading-[40px] md:leading-[40px] lg:leading-[48px] text-[#FDFDFD] mb-4 md:mb-6 tracking-[-0.02em] max-w-4xl font-['Inter Display'] ">
